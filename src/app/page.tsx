1f6217c592dba5c558d4d0396aab6c834eb06bd5
soorokim <soorokim@ent-bc.com>
2024-02-06 17:14:14 +0900

    cache clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { ArticleService } from "@/entity/article/api/service";
+import { articleService } from "@/entity/article/api/service";
 
 const HomePage = async () => {
-  const res = await ArticleService.getArticles();
+  const res = await articleService.getArticles();
 
   // console.log(res);
   return <div>{JSON.stringify(res)}</div>;
--- src/shared/ui/Checkbox.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type CheckboxProps = Omit<React.ComponentProps<"input">, "type">;
-
-const Checkbox = ({ ...props }: CheckboxProps) => {
-  return <input type="checkbox" {...props} />;
-};
-
-export default Checkbox;
